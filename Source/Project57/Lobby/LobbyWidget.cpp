01e3f3d9394321c8e5992c73c2b9daf0d9d8de55
junios-study <junios@naver.com>
2025-12-03 14:36:33 +0900

    로비 UI 기본 구조 작업

--- Source/Project57/Lobby/LobbyWidget.cpp
+++ Source/Project57/Lobby/LobbyWidget.cpp
@@ -2,4 +2,36 @@
 
 
 #include "LobbyWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/EditableTextBox.h"
+#include "Components/ScrollBox.h"
+#include "Components/Button.h"
 
+void ULobbyWidget::NativeOnInitialized()
+{
+	Super::NativeOnInitialized();
+
+	if (StartButton)
+	{
+		StartButton->OnClicked.AddDynamic(this, &ULobbyWidget::Start);
+	}
+
+	if (ChatInput)
+	{
+		ChatInput->OnTextCommitted.AddDynamic(this, &ULobbyWidget::ProcessOnCommit);
+		ChatInput->OnTextChanged.AddDynamic(this, &ULobbyWidget::ProcessOnChange);
+	}
+}
+
+void ULobbyWidget::Start()
+{
+
+}
+
+void ULobbyWidget::ProcessOnCommit(const FText& Text, ETextCommit::Type CommitMethod)
+{
+}
+
+void ULobbyWidget::ProcessOnChange(const FText& Text)
+{
+}
--- Source/Project57/Lobby/LobbyWidget.h
+++ Source/Project57/Lobby/LobbyWidget.h
@@ -6,12 +6,48 @@
 #include "Blueprint/UserWidget.h"
 #include "LobbyWidget.generated.h"
 
+class UButton;
+class UEditableTextBox;
+class UTextBlock;
+class UScrollBox;
+
 /**
- * 
+ *
  */
 UCLASS()
 class PROJECT57_API ULobbyWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+	virtual void NativeOnInitialized() override;
+
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Component, meta = (WidgetBind))
+	TObjectPtr<UButton> StartButton;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Component, meta = (WidgetBind))
+	TObjectPtr<UEditableTextBox> ChatInput;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Component, meta = (WidgetBind))
+	TObjectPtr <UTextBlock> ConnectionCount;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Component, meta = (WidgetBind))
+	TObjectPtr<UTextBlock> LeftTime;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Component, meta = (WidgetBind))
+	TObjectPtr<UScrollBox> ChatScrollBox;
+
+
+	UFUNCTION()
+	void Start();
+
+
+
+	UFUNCTION()
+	void ProcessOnCommit(const FText& Text, ETextCommit::Type CommitMethod);
+
+	UFUNCTION()
+	void ProcessOnChange(const FText& Text);
+
 };
